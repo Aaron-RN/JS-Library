8195588b35dcfdc2fd41624c3bf037275710e4f9
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-01-29 13:15:35 -0400

    Implemented basic book add/remove

--- js/main.js
+++ js/main.js
@@ -1,34 +1,45 @@
-let libraryDiv = document.getElementById("Library");
-let btnAddBook = document.getElementById("btnAddBook");
+const libraryDiv = document.getElementById('Library');
+const btnAddBook = document.getElementById('btnAddBook');
 let myLibrary = [];
 
-btnAddBook.addEventListener("click", addBookToLibrary);
+btnAddBook.addEventListener('click', addBookToLibrary);
 
 function Book(title, author, desc) {
-  // the constructor...
-    this.title = title;
-    this.author = author;
-    this.desc = desc;
+  this.id = Math.random().toString(36).substr(2, 9);
+  this.title = title;
+  this.author = author;
+  this.desc = desc;
 }
 
-function render(){
-    const html = myLibrary
-    .reduce((html, book) => {
-        `${html}
-        <div class="">${book.title}</div>
-        <div class="">${book.author}</div>
-        <div class="">${book.desc}</div>`
-        return html;
-    }, '');
-    console.log(html);
+const removeBook = (e) => {
+  const { bookId } = e.target.dataset;
+  myLibrary = myLibrary.filter(book => book.id !== bookId);
+  render();
+};
+
+const bookToHTML = (book) => `
+  <div class="book card">
+    <div class="card-body">
+      <h5 class="card-title">${book.title}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">By ${book.author}</h6>
+      <p class="card-text">${book.desc}</p>
+      <button class="card-link btn btn-danger btn-remove-book" data-book-id="${book.id}">Remove book</button>
+    </div>
+  </div>`;
+
+function render() {
+  const html = myLibrary.reduce((html, book) => `${html}${bookToHTML(book)}`, '');
+  libraryDiv.innerHTML = html;
+  const removeBookBtns = document.querySelectorAll('.btn-remove-book');
+  removeBookBtns.forEach(button => button.addEventListener('click', removeBook));
 }
 
 function addBookToLibrary() {
-    let bookTitle = document.getElementById("title");
-    let bookAuthor = document.getElementById("author");
-    let bookDesc = document.getElementById("desc");
+  const bookTitle = document.getElementById('title');
+  const bookAuthor = document.getElementById('author');
+  const bookDesc = document.getElementById('desc');
 
-    let book = new Book(bookTitle.value, bookAuthor.value, bookDesc.value);
-    myLibrary.push(book);
-    render();
+  const book = new Book(bookTitle.value, bookAuthor.value, bookDesc.value);
+  myLibrary.push(book);
+  render();
 }
