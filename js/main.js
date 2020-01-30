014fde33e0d07c9331a76aa810814fe8cabf8d7f
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-01-30 11:05:18 -0400

    Separated dom logic to dom.js file

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ const btnNewBook = document.getElementById('btnNewBook');
 const btnAddBook = document.getElementById('btnAddBook');
 let myLibrary = [];
 
-function Book(title, author, desc) {
+function Book(title, author, pages, desc) {
   this.constructor = Book;
   this.id = Math.random().toString(36).substr(2, 9);
   this.title = title;
   this.author = author;
+  this.pages = pages;
   this.desc = desc;
   this.wasRead = false;
 }
@@ -17,11 +18,15 @@ Book.prototype.toggleRead = function toogleRead() {
   this.wasRead = !this.wasRead;
 };
 
+const saveToLocalStorage = () => {
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+};
+
 /* eslint-disable no-use-before-define */
 const removeBook = (e) => {
   const { bookId } = e.target.dataset;
   myLibrary = myLibrary.filter(book => book.id !== bookId);
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  saveToLocalStorage();
   render();
 };
 
@@ -29,30 +34,10 @@ const readBook = (e) => {
   const { bookId } = e.target.dataset;
   const book = myLibrary.find(book => book.id === bookId);
   book.toggleRead();
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  saveToLocalStorage();
   render();
 };
 /* eslint-enable no-use-before-define */
-
-const bookToHTML = (book) => `
-  <div class="book col-md-6">
-    <div class="card shadow">
-      <div class="card-body">
-        <h5 class="card-title">${book.title}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">By ${book.author}</h6>
-        <p class="card-text">${book.desc}</p>
-        <button class="card-link btn btn-sm btn-read btn-${book.wasRead ? 'success' : 'secondary'}" data-book-id="${book.id}">
-          ${book.wasRead ? 'Mark as unread' : 'Mark as read'}
-        </button>
-        <button class="card-link btn btn-sm btn-danger btn-remove-book" data-book-id="${book.id}">Remove book</button>
-      </div>
-    </div>
-  </div>`;
-
-const errorToHTML = (error) => `
-  <div class="alert alert-danger" role="alert">
-    ${error}
-  </div>`;
 
 function render() {
   const html = myLibrary.reduce((html, book) => `${html}${bookToHTML(book)}`, '');
@@ -65,22 +50,25 @@ function render() {
 
 function addBookToLibrary() {
   const errors = [];
-  const bookTitle = document.getElementById('title');
-  const bookAuthor = document.getElementById('author');
-  const bookDesc = document.getElementById('desc');
+  const bookTitle = document.getElementById('title').value;
+  const bookAuthor = document.getElementById('author').value;
+  const bookPages = parseInt(document.getElementById('pages').value, 10);
+  const bookDesc = document.getElementById('desc').value;
 
-  if (!bookTitle.value) { errors.push('No Title Given!'); }
-  if (!bookAuthor.value) { errors.push('No Author Given!'); }
-  if (!bookDesc.value) { errors.push('No Description Given!'); }
+  if (!bookTitle) { errors.push('No Title Given!'); }
+  if (!bookAuthor) { errors.push('No Author Given!'); }
+  if (Number.isNaN(bookPages) || bookPages < 1) { errors.push('No valid Number of pages Given!'); }
+  if (!bookDesc) { errors.push('No Description Given!'); }
+
   if (errors.length > 0) {
     const html = errors.reduce((html, error) => `${html}${errorToHTML(error)}`, '');
     errorElem.innerHTML = html;
     return;
   }
-  const book = new Book(bookTitle.value, bookAuthor.value, bookDesc.value);
+  const book = new Book(bookTitle, bookAuthor, bookPages, bookDesc);
   myLibrary.push(book);
   errorElem.innerHTML = '';
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  saveToLocalStorage();
   render();
 }
 
